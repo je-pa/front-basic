bff7ebade45a428007be803a7ea5f11174e9f004
je-pa <jieun0502@gmail.com>
2022-02-04 17:29:12 +0900

    2022.02.04(금) scroll snap-x축 작업

--- src/main/resources/static/js/slide.js
+++ src/main/resources/static/js/slide.js
@@ -1,3 +1,39 @@
+let timer;
+const mainSections = document.querySelectorAll('main section');
+const lists = document.querySelectorAll('main ul li a');
+document.addEventListener('scroll',()=>{
+    if (timer) {
+        clearTimeout(timer);
+    }
+    timer = setTimeout(()=>{
+        const scrollValue = document.documentElement.scrollTop;
+        const sectionNum = mainSections.length;
+
+        mainSections.forEach((e, i, arr) => {
+            if(
+                ( !(i==0||i==sectionNum-1) && arr[i-1].offsetTop < scrollValue && scrollValue<mainSections[i+1].offsetTop ) || // 첫번째랑 마지막 제외
+                ( i==0 && 0<=scrollValue && scrollValue<mainSections[1].offsetTop ) || // 첫번째 - html 젤 위부터 section 두번째 TOP 까지
+                ( i==sectionNum-1 && mainSections[sectionNum-2].offsetTop < scrollValue && scrollValue < document.body.scrollHeight ) // 마지막 - section 밑에서 두번째 TOP 부터 html 바닥까지
+
+            ){
+                setColor(i);
+            }
+        })
+
+    },100)
+
+    function setColor(index){
+        for(let i=0; i<lists.length ; i++){
+            if(index != i){
+                lists[i].style.color='white';
+            }else{
+                lists[i].style.color='black';
+            }
+
+        }
+    }
+})
+
 // var element = document.getElementById("mine"); element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
 
 // document.addEventListener('scroll',()=>{
@@ -33,50 +69,14 @@
 //
 // })
 
-let timer;
-const mainSections = document.querySelectorAll('main section');
-const lists = document.querySelectorAll('main ul li a');
-document.addEventListener('scroll',()=>{
-    if (timer) {
-        clearTimeout(timer);
-    }
-    timer = setTimeout(()=>{
-        const scrollValue = document.documentElement.scrollTop;
-        console.log(scrollValue);
-        // for(let i=0; i<mainSections.length; i++){
-        //     if(i=0 && 0<=scrollValue && scrollValue<mainSections[i+1].offsetTop){
-        //         console.log(i);
-        //     }else if(i=mainSections.length-1 && mainSections[i-1].offsetTop < scrollValue && scrollValue < document.body.scrollHeight ){
-        //         console.log(i);
-        //     }else if(mainSections[i-1].offsetTop < scrollValue && scrollValue<mainSections[i+1].offsetTop){
-        //         console.log(i);
-        //     }
-        // }
-        if(0<=scrollValue && scrollValue<mainSections[1].offsetTop){
-            console.log(1);
-            setColor(0);
-        }else if(mainSections[0].offsetTop < scrollValue && scrollValue<mainSections[2].offsetTop){
-            console.log(2);
-            setColor(1);
-        }else if(mainSections[1].offsetTop < scrollValue && scrollValue<mainSections[3].offsetTop){
-            console.log(3);
-            setColor(2);
-        }else if(mainSections[2].offsetTop < scrollValue && scrollValue<mainSections[4].offsetTop){
-            console.log(4);
-            setColor(3);
-        }else if(mainSections[3].offsetTop < scrollValue && scrollValue < document.body.scrollHeight ){
-            console.log(5);
-            setColor(4);
-        }
-    },100)
 
-    function setColor(index){
-        for(let i=0; i<lists.length ; i++){
-            if(index != i){
-                lists[i].style.color='white';
-            }else{
-                lists[i].style.color='black';
-            }
-        }
-    }
-})
+
+// window.__scrollPosition = document.documentElement.scrollTop || 0;
+//
+// document.addEventListener('scroll', function() {
+//     let _documentY = document.documentElement.scrollTop;
+//     let _direction = _documentY - window.__scrollPosition >= 0 ? 1 : -1;
+//     console.log(_direction); // 콘솔창에 스크롤 방향을 출력
+//
+//     window.__scrollPosition = _documentY; // Update scrollY
+// });
